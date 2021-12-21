08e4fa40031ac68c244a6770ecf634368a047d71
bsrkynk <busra.kaynak@ogr.sakarya.edu.tr>
2021-12-21 16:28:08 +0300

    [BUSRA]  Setting Screen Frontend edited

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,36 +1,66 @@
 import { getAuth, signOut } from 'firebase/auth';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
-
-
 
 const handleSignOut = (auth) => {
     signOut(auth)
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(error.message);
-    })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(error.message);
+        })
 }
 
 const Settings = () => {
     const navigation = useNavigation();
     const auth = getAuth();
     return (
-        <View style = {styles.container}>
-            <Text>
-                Email: {auth.currentUser?.email}
-            </Text>
-            <TouchableOpacity
-                onPress={()=>{handleSignOut(auth);navigation.replace('Login')}}
-                style={styles.button}
-            >
-                <Text style = {styles.buttonText}>
-                    Sign Out
-                </Text>
-            </TouchableOpacity>
+
+        <View >
+            <ScrollView>
+                <View style={{
+                    backgroundColor: 'black',
+                    padding: 10,
+                    width: '100%',
+                    height: 200
+                }}>
+                    <View></View>
+                    <View></View>
+                </View>
+                <View style={{ alignItems: 'center' }} >
+                    <Image source={require('../images/Login.jpeg')} style={{ width: 200, height: 200, borderRadius: 100, marginTop: -70 }}></Image>
+                    <Text style={{ fontSize: 25, fontWeight: 'bold', padding: 10 }} > User </Text>
+                </View>
+
+                <View style={{
+                    alignSelf: 'center',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    backgroundColor: 'grey',
+                    width: '90%',
+                    padding: 20,
+                    paddingBottom: 22,
+                    borderRadius: 10,
+                    elevation: 15,
+                    marginTop: 20,
+                    shadowOpacity: 80
+                }} >
+                    <Text style={{ fontWeight: 'bold' }} >
+                        Email: {auth.currentUser?.email}
+                    </Text>
+                </View>
+                <TouchableOpacity
+                    onPress={() => { handleSignOut(auth); navigation.replace('Login') }}
+                    style={styles.button}
+                >
+                    <Text style={styles.buttonText}>
+                        Sign Out
+                    </Text>
+                </TouchableOpacity>
+
+            </ScrollView>
         </View>
     )
 }
@@ -38,28 +68,29 @@ const Settings = () => {
 export default Settings
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-    button:{
-        backgroundColor:'orange',
-        width:'60%',
-        padding:15,
-        borderRadius:10,
-        alignItems:'center',
-        marginTop:40,
+    button: {
+        height: 60,
+        marginTop: 40,
+        marginHorizontal: 20,
+        backgroundColor: 'orange',
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
-    buttonOutline:{
-        backgroundColor:'white',
-        marginTop:5,
-        borderColor:'blue',
-        borderWidth:2,
+    buttonOutline: {
+        backgroundColor: 'white',
+        marginTop: 5,
+        borderColor: 'blue',
+        borderWidth: 2,
     },
-    buttonText:{
-        color:'white',
-        fontWeight:'700',
-        fontSize:16,
+    buttonText: {
+        color: 'white',
+        fontWeight: '700',
+        fontSize: 16,
     },
 })
